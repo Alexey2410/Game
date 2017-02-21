4bbae9003955562f2f09470e5926a7fd75010c42
Alexey2410 <alexey2410@gmail.com>
2017-02-21 17:54:59 +0200

    new point

--- app/src/main/java/com/example/alex/game/CanvasView.java
+++ app/src/main/java/com/example/alex/game/CanvasView.java
@@ -3,13 +3,18 @@ package com.example.alex.game;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 
 /**
  * Created by APoddubenskiy on 2/20/2017.
  */
 public class CanvasView extends View {
+    private static int weight;
+    private static int height;
     private GameManager gameManager;
 
     public CanvasView(Context context) {
@@ -18,7 +23,17 @@ public class CanvasView extends View {
 
     public CanvasView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initWeightAndHeight(context);
         gameManager = new GameManager();
+    }
+
+    private void initWeightAndHeight(Context context) {
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        Point point = new Point();
+        display.getSize(point);
+        weight = point.x;
+        height = point.y;
     }
 
     public CanvasView(Context context, AttributeSet attrs, int defStyleAttr) {
