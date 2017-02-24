0c4e0ba88a998ce4f18b0d4bd2d4b95f443e7311
Alexey2410 <alexey2410@gmail.com>
2017-02-24 16:15:18 +0200

    add color for circles

--- app/src/main/java/com/example/alex/game/MainCircle.java
+++ app/src/main/java/com/example/alex/game/MainCircle.java
@@ -1,5 +1,7 @@
 package com.example.alex.game;
 
+import android.graphics.Color;
+
 /**
  * Created by APoddubenskiy on 2/20/2017.
  */
@@ -7,9 +9,11 @@ public class MainCircle extends SimpleCircle {
 
     public static final int RADIUS = 50;
     public static final int MAIN_SPEED = 30;
+    public static final int OUR_COLOR = Color.BLUE;
 
     public MainCircle(int x, int y) {
         super(x, y, RADIUS);
+        setColor(OUR_COLOR);
     }
 
     public void moveMainCircleWhenTouchAt(int x1, int y1) {
